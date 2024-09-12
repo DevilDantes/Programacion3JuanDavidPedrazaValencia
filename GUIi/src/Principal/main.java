5fb70b4a7ec2759f2e407370e35f0dc4db979e7f
DevilDantes <160239574+DevilDantes@users.noreply.github.com>
2024-09-12 12:02:05 -0500

    Add files via upload

---
+++ GUIi/src/Principal/main.java
@@ -0,0 +1,12 @@
+package Principal;
+
+import Formularios.FrmEstudiantes;
+
+public class main {
+    public static void main(String[] args) {
+        FrmEstudiantes E = new FrmEstudiantes();
+        
+        E.setVisible(true);
+    }
+    
+}
