75872a3493ec6005bd8e7692690afa9daa7cdbb0
DevilDantes <160239574+DevilDantes@users.noreply.github.com>
2024-09-19 11:53:53 -0500

    Add files via upload

---
+++ Loginn/src/Loginn/main.java
@@ -0,0 +1,12 @@
+package Loginn;
+
+import Formularios.FrmLogin;
+
+public class main {
+
+    public static void main(String[] args) {
+        FrmLogin E = new FrmLogin();
+        E.setVisible(true);
+    }
+    
+}
